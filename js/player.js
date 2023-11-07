ebfd12d8c35c0f15200231187f8f572c35b898fb
iamchrisas <chrisas.agb@gmail.com>
2023-11-07 20:57:50 +0100

    ok

---
+++ js/player.js
@@ -0,0 +1,62 @@
+class Player {
+    constructor(gameScreen, left, top, width, height, imgSrc){
+        this.gameScreen = gameScreen;
+        this.left = left;
+        this.top = top;
+        this.width = width;
+        this.height = height;
+        this.directionX = 0;
+        this.directionY = 0;
+        this.element = document.createElement("img");
+
+        this.element.src = imgSrc;
+        this.element.style.position = "absolute";
+        this.element.style.width = `${width}px`;
+        this.element.style.height = `${height}px`;
+        this.element.style.left = `${left}px`;
+        this.element.style.top = `${top}px`;
+
+        this.gameScreen.appendChild(this.element);
+    }
+
+    move() {
+        this.left += this.directionX;
+        this.top += this.directionY;
+
+        if (this.left + this.width >= this.gameScreen.offsetWidth){
+            this.left = this.gameScreen.offsetWidth - this.width;
+        } else if (this.left <= 0){
+            this.left = 0;
+        }
+
+        if (this.top <= 0){
+            this.top = 0;
+        }
+        else if (this.top + this.height >= this.gameScreen.offsetHeight){
+            this.top = this.gameScreen.offsetHeight - this.height;
+        }
+
+        this.updatePosition();
+    }
+
+    updatePosition() {
+        this.element.style.left = `${this.left}px`;
+        this.element.style.top = `${this.top}px`;
+    }
+
+    didCollide(obstacle) {
+        const playerRect = this.element.getBoundingClientRect();
+        const obstacleRect = obstacle.element.getBoundingClientRect();
+
+        if (
+            playerRect.left < obstacleRect.right &&
+            playerRect.right > obstacleRect.left &&
+            playerRect.top < obstacleRect.bottom &&
+            playerRect.bottom > obstacleRect.top
+        ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
